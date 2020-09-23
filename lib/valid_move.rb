abae046ae72ba7b502dd352fea9ad8fcb24c0d48
June Young Kang <jkang34@gmail.com>
2020-09-23 18:33:38 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@ def valid_move?(board,index)
   position = index.to_i - 1
 end
 
-def position_taken?(board, position)
+def position_taken?(board = [" ", position)
   if board[position] < 0 || board[position] > 9
     false
   else 
